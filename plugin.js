183db9b81b7be3cf5f102764065d22680fbe4b05
Michael Elliott <mielliott@ufl.edu>
2020-08-03 15:30:57 -0400

    added command for retrieving list of available packages

--- plugin.js
+++ plugin.js
@@ -1,24 +1,46 @@
-// SAGE2 is available for use under the SAGE2 Software License
-//
-// University of Illinois at Chicago's Electronic Visualization Laboratory (EVL)
-// and University of Hawai'i at Manoa's Laboratory for Advanced Visualization and
-// Applications (LAVA)
-//
-// See full text, terms and conditions in the LICENSE.txt included file
-//
-// Copyright (c) 2015
 
 "use strict";
 
 // built-in path module
-var path    = require('path');
-// load modules from the server's folder
-var request = require(path.join(module.parent.exports.dirname, 'request'));  // HTTP client request
-var $       = require(path.join(module.parent.exports.dirname, 'cheerio'));  // HTML parsing, jquery style
+var path    = require("path");
 
+// load modules from the server's folder
+var request = require(path.join(module.parent.exports.dirname, "request"));  // HTTP client request
+var $       = require(path.join(module.parent.exports.dirname, "cheerio"));  // HTML parsing, jquery style
+var fs		= require(path.join(module.parent.exports.dirname, "fs"));
 
 function processRequest(wsio, data, config) {
+	let cmd = data.query.cmd;
+	if (cmd) {
+		switch (cmd) {
+			case "listPackages":
+				let packagePaths = listPackages()
+				wsio.emit("broadcast", {
+					app: data.app,
+					func: data.func,
+					data: {
+						packages: packagePaths,
+						err: null
+					}
+				})
+				break;
+			default:
+				console.log(`Unrecognized command ${cmd}`);
+		}
+	}
+}
+
+function listPackages() {
+	let searchPath = path.join(mediaFolders.user.path, "collections", "/")
+	return fs.readdirSync(searchPath, {withFileTypes: true})
+		.filter(file => file.isDirectory())
+		.filter(file => directoryIsResultsPackage(path.join(searchPath, file.name)))
+		.map(file => file.name)
+}
+
+function directoryIsResultsPackage(dirPath) {
+    let files = fs.readdirSync(dirPath)
+    return (files.indexOf("package_ancPam.js") >= 0)
 }
 
 module.exports = processRequest;
-
