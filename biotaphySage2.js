57138d5bdf67ca5199d6206e818235bb9f9ecd6e
Michael Elliott <mielliott@ufl.edu>
2020-08-03 21:35:33 -0400

    added remote() to allow applets issue RPCs

--- biotaphySage2.js
+++ biotaphySage2.js
@@ -3,6 +3,10 @@
 // by: Michael Elliott <mielliott@ufl.edu>
 //
 
+"use strict";
+
+var uuid = require("uuid");
+
 var biotaphySage2 = SAGE2_App.extend({
     init: function (data) {
         // Create a canvas node for this app. Accessible later as this.element
@@ -11,11 +15,96 @@ var biotaphySage2 = SAGE2_App.extend({
         this.resizeEvents = "continuous";
         // Get the canvas context
         this.ctx = this.element.getContext("2d");
+
+        this.applet = null
+        this.package = null
+
+        this.pendingRequests = new Map();
+
+        if (data.customLaunchParams) {
+            this.loadApplet(data.customLaunchParams.appletName);
+        }
+
+        if (this.applet == null) {
+            this.loadApplet("Package")
+        }
     },
 
-    draw: function (data) {},
+    loadApplet: function(appletName) {
+        if (window[appletName] === undefined) {
+            this.initAndLoadApplet(appletName)
+        }
+        else {
+            this.loadInitializedApplet(appletName)
+        }
+    },
 
-    resize: function (date) {},
+    initAndLoadApplet: function(appletName) {
+        var path = require("path")
+        var appletsDirectory = path.join(this.resrcPath, "applets", "/");
+        var appletPath = path.join(appletsDirectory, `applet${appletName}.js`);
+
+        var js = document.createElement("script");
+
+        js.addEventListener('error', function(event) {
+            this.log(`Error loading applet ${appletName}`);
+        }, false);
+        
+        var _this = this
+        js.addEventListener('load', function(event) {
+            _this.loadInitializedApplet(appletName);
+        });
+
+        js.type = "text/javascript";
+        js.async = false;
+        js.src = appletPath;
+
+        this.element.appendChild(js);
+    },
+
+    remote: function(cmd, callback, _data, _bind) {
+        var requestId = uuid();
+        this.pendingRequests[requestId] = callback.bind(_bind ? _bind : this.applet);
+        console.log(_bind)
+
+        var query = {
+            cmd: cmd,
+            requestId: requestId
+        }
+		wsio.emit('applicationRPC', {
+            app: this.div.id,
+            func: "_remoteCallback",
+            query: query,
+            broadcast: true
+        });
+    },
+
+    _remoteCallback: function(response) {
+        var callback = this.pendingRequests[response.requestId];
+        delete response.requestId
+        if (callback) {
+            callback(response);
+        }
+    },
+
+    loadInitializedApplet: function(appletName) {
+        // TODO: do we need to do anything to clean up existing applets?
+        var applet = new window[`applet${appletName}`]();
+        this.applet = applet
+        applet.init(this);
+    },
+
+    draw: function (data) {
+        if (this.applet != null) {
+            this.applet.draw()
+        }
+    },
+
+    resize: function (date) {
+        if (this.applet != null) {
+            this.applet.draw()
+        }
+    },
 
     event: function (type, position, user, data, date) {},
 
--- plugin.js
+++ plugin.js
@@ -3,39 +3,41 @@
 
 // built-in path module
 var path    = require("path");
+var fs      = require("fs")
 
 // load modules from the server's folder
 var request = require(path.join(module.parent.exports.dirname, "request"));  // HTTP client request
-var $       = require(path.join(module.parent.exports.dirname, "cheerio"));  // HTML parsing, jquery style
-var fs		= require(path.join(module.parent.exports.dirname, "fs"));
+var $       = require("cheerio") // require(path.join(module.parent.exports.dirname, "cheerio"));  // HTML parsing, jquery style
+// var fs		= require(path.join(module.parent.exports.dirname, "fs"));
 
 function processRequest(wsio, data, config) {
-	let cmd = data.query.cmd;
-	if (cmd) {
-		switch (cmd) {
-			case "listPackages":
-				let packagePaths = listPackages()
-				wsio.emit("broadcast", {
-					app: data.app,
-					func: data.func,
-					data: {
-						packages: packagePaths,
-						err: null
-					}
-				})
-				break;
-			default:
-				console.log(`Unrecognized command ${cmd}`);
-		}
-	}
+    let cmd = data.query.cmd;
+    if (cmd) {
+        switch (cmd) {
+            case "listPackages":
+                let packagesList = listPackages()
+                wsio.emit("broadcast", {
+                    app: data.app,
+                    func: data.func,
+                    data: {
+                        requestId: data.query.requestId,
+                        packagesList: packagesList,
+                        err: null
+                    }
+                })
+                break;
+            default:
+                console.log(`Unrecognized command ${cmd}`);
+        }
+    }
 }
 
 function listPackages() {
-	let searchPath = path.join(mediaFolders.user.path, "collections", "/")
-	return fs.readdirSync(searchPath, {withFileTypes: true})
-		.filter(file => file.isDirectory())
-		.filter(file => directoryIsResultsPackage(path.join(searchPath, file.name)))
-		.map(file => file.name)
+    let searchPath = path.join(mediaFolders.user.path, "collections", "/")
+    return fs.readdirSync(searchPath, {withFileTypes: true})
+        .filter(file => file.isDirectory())
+        .filter(file => directoryIsResultsPackage(path.join(searchPath, file.name)))
+        .map(file => file.name)
 }
 
 function directoryIsResultsPackage(dirPath) {
