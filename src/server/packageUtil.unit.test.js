698dc8ab0bfe0a7b7cb7dee85139804f174b9c27
Michael Elliott <mielliott@ufl.edu>
2020-09-25 14:05:01 -0400

    Test failure behavior

--- src/server/packageUtil.unit.test.js
+++ src/server/packageUtil.unit.test.js
@@ -19,6 +19,23 @@ test("Get an absolute path to a file within a package", () => {
     expect(fullPath).toBe("pathToCollectionsFolder/collections/wombat/species_wombat.txt")
 })
 
+test("Fail to read missing data file", () => {
+    let readData = packageUtil.readDataFile("haha you're not real")
+    expect(readData).toBeNull()
+})
+
+test("Fail to read non-data file", () => {
+    const path = require("path")
+    global.mediaFolders = {
+        user : {
+            path : path.resolve("test/resources/")
+        }
+    }
+
+    let readData = packageUtil.readDataFile(packageUtil.getFullPath("wombat", "images", "lm_logo.png"))
+    expect(readData).toBeNull()
+})
+
 test("Read data from a BiotaPhy data file", () => {
     const path = require("path")
     global.mediaFolders = {
